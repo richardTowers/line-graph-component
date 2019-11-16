f312fce908fcfb9ebaaa71d51a9f9f489460c86b
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-11-16 17:08:28 +0000

    Add the overlay in HTML so it scales more nicely
    Needs some serious tidying!!

--- src/Path.tsx
+++ src/Path.tsx
@@ -6,17 +6,18 @@ export default function Path(props: {points: [number, number][]}) {
   const line = `M${scaledPoints.map(p => `${p[1]},${p[0]}`).join('L')}`;
 
   return (
-    <svg className='govuk-line-data-path' viewBox='0 0 960 320' preserveAspectRatio='none'>
-      <path d={line} />
-      {scaledPoints.map((p,i) => (
-        // @ts-ignore
-        <a className='govuk-line-data-point-overlay' xlinkHref={`#point-${i}`} href={`#point-${i}`}>
-          <line x1={p[1]} y1={25} x2={p[1]} y2={320}/>
-          <circle cx={p[1]} cy={p[0]} r={5}/>
-          <rect x={p[1]-10} y={0} width={20} height={320}/>
-          <text x={p[1] - 20} y={20}>{(100 - p[0]/3.2).toFixed(2) + '%'}</text>
-        </a>
-      ))}
-    </svg>
+    <div>
+      <svg className='govuk-line-data-path' viewBox='0 0 960 320' preserveAspectRatio='none'>
+        <path d={line} />
+      </svg>
+      <div>
+        {scaledPoints.map((p,i) => (
+          <a className='govuk-line-data-point-overlay' href={`#point-${i}`} style={{left: `calc((100% - 4rem) * ${i}/50 + 4rem)`}}>
+            <span className='govuk-line-data-point-overlay-value'>{(100 - p[0]/3.2).toFixed(2)}%</span>
+            <span className='govuk-line-data-point-overlay-marker' style={{top: `calc(${p[0]/3.2}% - 5px)`}}></span>
+          </a>
+        ))}
+      </div>
+    </div>
   );
 }
