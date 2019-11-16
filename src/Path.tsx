88e73b2c38db0f80aa85ad85b71b029f1ad9d3db
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-11-16 16:08:36 +0000

    Hacky interactivity
    Note: the `<a>` tag really needs an xlink:href, but react / typescript
    are fighting me, so it's too difficult to make that happen.

--- src/Path.tsx
+++ src/Path.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import './Path.css';
 
 export default function Path(props: {points: [number, number][]}) {
-  const line = `M${props.points.map(pair => pair[1] + ',' + pair[0]).join('L')}`;
+  const scaledPoints = props.points.map(p => [p[0] * 3.2, p[1] * 9.6]);
+  const line = `M${scaledPoints.map(p => `${p[1]},${p[0]}`).join('L')}`;
 
   return (
-    <svg className='govuk-line-data-path' viewBox='0 0 100 100' preserveAspectRatio='none'>
+    <svg className='govuk-line-data-path' viewBox='0 0 960 320' preserveAspectRatio='none'>
       <path d={line} />
+      {scaledPoints.map((p,i) => (
+        // @ts-ignore
+        <a className='govuk-line-data-point-overlay' xlinkHref={`#point-${i}`} href={`#point-${i}`}>
+          <line x1={p[1]} y1={25} x2={p[1]} y2={320}/>
+          <circle cx={p[1]} cy={p[0]} r={5}/>
+          <rect x={p[1]-10} y={0} width={20} height={320}/>
+          <text x={p[1] - 20} y={20}>{(100 - p[0]/3.2).toFixed(2) + '%'}</text>
+        </a>
+      ))}
     </svg>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -9,12 +9,19 @@ function randomWalk(x0: number, y0: number, n: number): [number, number][] {
   let y = y0;
   points[0] = [x, y];
   for (let i = 1; i < n; i++) {
-    x += (Math.random() - 0.5) * 10;
+    x += (Math.random() - 0.5) * 20;
     x = x > 100 ? 100 : x;
     x = x < 0 ? 0 : x;
-    y += 1;
+    y += 2;
     points[i] = [x, y];
   }
   return points;
 }
-ReactDOM.render(<FullLineGraph points={randomWalk(50, 0, 100)}/>, document.getElementById('root'));
+ReactDOM.render([
+  <p>
+    An experiment in making an interactive SVG line graph using pure HTML, CSS and SVG (i.e. without requiring client side JS).
+  </p>,
+  <p>Currently this actually is using client side JS to render the chart, but that's just for convenience.</p>,
+  <p>The actual interactions are all native HTML / CSS. Try tabbing through some links to see!</p>,
+  <FullLineGraph points={randomWalk(50, 0, 50)}/>
+], document.getElementById('root'));
