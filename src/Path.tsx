382853f40b9bd010d7354ee85a59ff986ad3cb22
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-11-17 16:49:23 +0000

    Don't scale points, use 100x100 viewBox
    Since the SVG is only being used to draw lines and paths which have
    `vector-effect: non-scaling-stroke;` set we don't need to set a viewBox
    that's anything like the real aspect ratio of the image.
    
    This means we can scale the data we're showing to a nice simple 0-100
    range. In the case of the stub data it's in that range anyway, so no
    need for the scaling code for now - it would be needed in the real world
    though.

--- src/Path.tsx
+++ src/Path.tsx
@@ -2,19 +2,18 @@ import React from 'react';
 import './Path.css';
 
 export default function Path(props: {points: [number, number][]}) {
-  const scaledPoints = props.points.map(p => [p[0] * 3.2, p[1] * 9.6]);
-  const line = `M${scaledPoints.map(p => `${p[1]},${p[0]}`).join('L')}`;
+  const line = `M${props.points.map(p => `${p[1]},${p[0]}`).join('L')}`;
 
   return (
     <div>
-      <svg className='govuk-line-data-path' viewBox='0 0 960 320' preserveAspectRatio='none'>
+      <svg className='govuk-line-data-path' viewBox='0 0 100 100' preserveAspectRatio='none'>
         <path d={line} />
       </svg>
       <div>
-        {scaledPoints.map((p,i) => (
+        {props.points.map((p,i) => (
           <a className='govuk-line-data-point-overlay' href={`#point-${i}`} style={{left: `calc((100% - 4rem) * ${i}/50 + 4rem)`}}>
-            <span className='govuk-line-data-point-overlay-value'>{(100 - p[0]/3.2).toFixed(2)}%</span>
-            <span className='govuk-line-data-point-overlay-marker' style={{top: `calc(${p[0]/3.2}% - 5px)`}}></span>
+            <span className='govuk-line-data-point-overlay-value'>{(100 - p[0]).toFixed(2)}%</span>
+            <span className='govuk-line-data-point-overlay-marker' style={{top: `calc(${p[0]}% - 5px)`}}></span>
           </a>
         ))}
       </div>
